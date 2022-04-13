9ad443c3993a44f7b447e7541f6c63e807bb9af4
Tomás Payer <91624465+TomasPayer@users.noreply.github.com>
2022-04-13 00:57:28 -0300

    ExpressAvanzado

--- express.js
+++ express.js
@@ -1,26 +1,90 @@
-const { Console } = require('console');
 const express = require('express');
 
 const app = express();
 const PORT = 8080;
-let cantidaVisitas = 0;
 
 const server = app.listen(PORT, () => {
-    console.log("Aplicacion express escuchando en el puerto 8080");
-})
+    console.log("Aplicacion express escuchando en puerto 8080");
+});
 
-server.on("Error", error => console.log('Se tiene el siguiente error : ${error}'));
+app.use(express.json());
+app.use(express.urlencoded({ extended : true }));
 
+
+//GET
 app.get('/', (req, resp) => {
-    resp.send({mensaje: 'Hola mundo!'});
+    resp.send({mensaje: 'Ruta de Inicio'});
 })
 
-app.get('/visitas', (req, resp) => {
-    cantidaVisitas++;
-    resp.send({cantidad: cantidaVisitas});
-})
+app.get('/api/productos', (req, resp) => {
+    let productos = [{nombre:'heladera', precio:100, id:5}, {nombre:'heladera2', precio:200, id:6}];
+    //contenedor --> getById(req.params.id)
 
-app.get('/hola', (req, resp) => {
-    resp.send({mensaje: 'Hola tomas!'});
-})
+    if(Object.entries(req.query).length > 0) {
+        resp.json({
+        result: 'geat all WITH params',
+        productos: productos,
+        query: req.query
+    });
+    } else {
+      resp.json({
+           result: 'get all without params',
+           productos: productos
+        });
+       }
+      })
+
+      app.get('/api/productos/:id', (req, resp) => {
+        let productos = [{nombre:'heladera', precio:100, id:5}, {nombre:'heladera2', precio:200, id:6}];
+        //contenedor --> getById(req.params.id)
+    
+            resp.json({
+            result: 'geat product by id',
+            producto: productos[req.params.id],
+            id: req.params.id
+        });
+    })
+    
+
+      //POST (no tiene id)
+      app.post('/api/productos', (req, resp) => {
+          //logica let NewProduct = req.body
+        resp.json({
+            result: 'Save product',
+            body: req.body
+        });
+    })
+
+
+    //PUT
+      app.put('/api/productos/:id', (req, resp) => {
+        //let productos = [{nombre:'heladera', precio:100}, {nombre:'heladera2', precio:200}];
+        //logica
+            resp.json({
+            result: 'edit by id',
+            id: req.params.id,
+            body: req.body
+        });
+    })
+
+           //DELETE
+    app.delete('/api/productos/:id', (req, resp) => {
+        //let productos = [{nombre:'heladera', precio:100}, {nombre:'heladera2', precio:200}];
+        //logica
+            resp.json({
+            result: 'edit by id',
+            id: req.params.id,
+        });
+    })
+
+
+         //EJERCICIO DE PRACTICA
+      const frase = 'Hola mi nombre es Tomas Payer'
+
+      app.get('/api/frase', (req, resp) => {
+          resp.json({
+              resul: 'Se devuelve la frase',
+              frase: frase
+          })
+      })
 
