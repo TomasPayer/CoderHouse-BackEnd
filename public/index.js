ed8ce73e87ccdc5ad35fe8011cf06066f1f0ed44
Tomás Payer <91624465+TomasPayer@users.noreply.github.com>
2022-04-25 23:32:22 -0300

    changes

--- public/index.js
+++ public/index.js
@@ -1,5 +1,19 @@
+//Client view
 const socket = io();
 
-socket.on('my message', data => {
-    console.log(data);
+let button = document.getElementById('btn-send');
+let inputMensaje = document.getElementById('message');
+let parrafo = document.getElementById('parrafo');
+
+button.addEventListener('click', function (params) {
+    let value = inputMensaje.value;
+    
+    socket.emit('message', value);
 })
+
+
+socket.on('my message', data => {
+    parrafo.innerHTML += '</br>' + data;
+    console.log(data);
+    socket.emit('notification', 'Message received from the client');
+});
--- server.js
+++ server.js
@@ -1,23 +1,30 @@
-const { Socket } = require('engine.io');
 const express = require('express');
 const { Server: HttpServer } = require('http');
-const { Server: IOServer } = require('socket.io');
+const { Server:IOServer } = require('socket.io');
 
 const app = express();
 const httpServer = new HttpServer(app);
 const io = new IOServer(httpServer);
 
+let mensajes = [];
 
 app.use(express.static('./public'));
-    
-    app.get('/', (req, res) => {
-        res.sendFile('index.html', { root: __dirname });
-    });
 
-    httpServer.listen(3000, () => console.log('SERVER ON'))
+app.get('/', (req, res) => {
+    res.sendFile('index.html', {root: __dirname});
+});
 
-    io.on('connection', (socket) => {
-          console.log('Usuario conectado');
-          socket.emit('my message', 'Hi Tomas'); 
-        });
-      
+httpServer.listen(3000, () => console.log('SERVER ON'));
+
+io.on('connection', (socket) => {
+    console.log('Logged in user');
+    socket.emit('my message', 'Hi from the server');
+
+    socket.on('notification', info => {
+        console.log(info);
+    })
+
+    socket.on('message', data => {
+        io.sockets.emit('my message', data);
+    })
+});
